0dd58c4b7835933aa1c83cf29bd608929734f5da
Carlo Santos <carlo@snapit.solutions>
2020-08-25 12:24:00 -0500

    pushing up a bulb excercise

--- js/basic.js
+++ js/basic.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+function on_bulb() {
+  document.getElementById('bulbImg').src='images/bulbon.jpg';
+}
+
+function off_bulb() {
+  document.getElementById('bulbImg').src='images/bulboff.jpg';
+}
